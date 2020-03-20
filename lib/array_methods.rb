634bd6ab92ca9255b57a9e4ce1078d75960f46f5
Stephanie Cheney <steph@scheneyphotography.com>
2020-03-20 16:36:43 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,7 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  x = 0
+  x = array[0]
   array.length.times do |index|
   if array[index] > x 
       x = array [index]
@@ -13,7 +13,7 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  x = 0
+  x = array[0]
   array.length.times do |index|
   if array[index] < x 
       x = array [index]
